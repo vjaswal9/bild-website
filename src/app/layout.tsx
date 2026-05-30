7ece9d27d411701cdef3c3ae81eb6c5d58838755
Vijay Jaswal <vijay@bild.ae>
2026-05-30 21:10:48 +0400

    Increase BILD logo to 96px, navbar height to 112px

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className="min-h-screen flex flex-col">
         <Navbar />
-        <main className="flex-1 pt-20">{children}</main>
+        <main className="flex-1 pt-28">{children}</main>
         <Footer />
       </body>
     </html>
